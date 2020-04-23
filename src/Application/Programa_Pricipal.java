354a1f1edced7346001c2ab3c5286fc24c354da2
Denilson Neres <dnsj12jc@gmail.com>
2020-04-23 20:29:17 -0300

    Primeira classe: Posição

--- src/Application/Programa_Pricipal.java
+++ src/Application/Programa_Pricipal.java
@@ -1,10 +1,12 @@
 package Application;
 
+import JogoTabuleiro.Posicao;
+
 public class Programa_Pricipal {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		Posicao pos = new Posicao(3, 5);
+		System.out.println(pos);
 	}
 
 }
